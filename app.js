0df907130b053b3625cba22bd37436e3685d90a6
kerokero014 <kevinjrm.kr@gmail.com>
2024-02-07 17:27:12 -0700

    Add job application tracker and connect to MongoDB

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
+//Job application Tracker Project
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
 
+//Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
+//Test route
 app.get('/', (req, res) => {
     res.send('Hello World');
     }
 );
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+
+// Start server + connect to DB
+mongodb.initDb((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      app.listen(port);
+      console.log(`Connected to DB and listening on ${port}`);
     }
-);
+  });
 
 
