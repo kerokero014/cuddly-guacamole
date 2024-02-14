204751cd505ea557aa84271db2f32a979fc12133
kerokero014 <kevinjrm.kr@gmail.com>
2024-02-14 15:26:55 -0700

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,9 +7,9 @@ router.get('/', userController.getUsers); //get all
 
 router.get('/:id', userController.getSingleUser); // get one
 
-router.post('/', UserValidation.userRules ,userController.createNewUser); // create one
+router.post('/', UserValidation.userRules, userController.createNewUser); // create one
 
-router.put('/:id', UserValidation.userRules ,userController.updateUser); // update one
+router.put('/:id', UserValidation.userRules, userController.updateUser); // update one
 
 router.delete('/:id', userController.deleteUser); // delete one
 
---
+++ utils/app-validation.js
@@ -0,0 +1,49 @@
+const body = require('express-validator');
+
+const appValidation = () => {
+  return [
+    body('Job')
+      .notEmpty()
+      .withMessage('Job field is required')
+      .isString()
+      .withMessage('Job must be a string'),
+    body('Company')
+      .notEmpty()
+      .withMessage('Company field is required')
+      .isString()
+      .withMessage('Company must be a string'),
+    body('Location')
+      .notEmpty()
+      .withMessage('Location field is required')
+      .isString()
+      .withMessage('Location must be a string'),
+    body('Salary')
+      .notEmpty()
+      .withMessage('Salary field is required')
+      .isNumeric()
+      .withMessage('Salary must be a number'),
+    body('Benefits').optional().isArray().withMessage('Benefits must be an array'),
+    body('Experience')
+      .notEmpty()
+      .withMessage('Experience field is required')
+      .isString()
+      .withMessage('Experience must be a string'),
+    body('Skills').optional().isArray().withMessage('Skills must be an array'),
+    body('Description').optional().isString().withMessage('Description must be a string'),
+    body('website').optional().isURL().withMessage('Invalid website URL'),
+    body('interviewDate').optional().isISO8601().toDate().withMessage('Invalid interview date'),
+    body('interviewTime').optional().isString().withMessage('Interview time must be a string'),
+    body('applicationStatus')
+      .optional()
+      .isString()
+      .withMessage('Application status must be a string'),
+    body('interviewStatus').optional().isString().withMessage('Interview status must be a string'),
+    body('offerStatus').optional().isString().withMessage('Offer status must be a string'),
+    body('interviewFeedback')
+      .optional()
+      .isString()
+      .withMessage('Interview feedback must be a string')
+  ];
+};
+
+module.exports = { appValidation };
