aab613de102166b4917886baccda3553814ae99f
kerokero014 <kevinjrm.kr@gmail.com>
2024-02-14 14:10:42 -0700

    Refactor userController.js and routes.rest

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,4 @@
 const User = require('../models/userModel');
-const userData = require('../models/userSchema');
 
 const getUsers = async (req, res) => {
   try {
@@ -31,7 +30,18 @@ const getSingleUser = async (req, res) => {
 
 const createNewUser = async (req, res) => {
   try {
-    const userData = req.body;
+    const userData = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      age: req.body.age,
+      email: req.body.email,
+      phone: req.body.phone,
+      jobTitle: req.body.jobTitle,
+      experience: req.body.experience,
+      education: req.body.education,
+      password: req.body.password
+    };
+
     const newUser = await User.create(userData);
 
     res.status(201).json({ message: 'User created successfully', user: newUser });
@@ -44,7 +54,17 @@ const createNewUser = async (req, res) => {
 const updateUser = async (req, res) => {
   try {
     const userId = req.params.id;
-    const userData = req.body;
+    const userData = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      age: req.body.age,
+      email: req.body.email,
+      phone: req.body.phone,
+      jobTitle: req.body.jobTitle,
+      experience: req.body.experience,
+      education: req.body.education,
+      password: req.body.password
+    };
     
     const updatedUser = await User.update(userId, userData);
 
