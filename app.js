be1ad8fffac078b32957470f5e99af35b6c75daa
kerokero014 <kevinjrm.kr@gmail.com>
2024-02-19 16:05:45 -0700

    Add Auth0 authentication middleware

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 const port = process.env.PORT || 8080;
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const { initDb } = require('./db/connect'); // Import the initDb function from your Mongoose connection file
 const routes = require('./routes');
+const { auth, requiresAuth } = require('express-openid-connect');
+
+// Auth0
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/', routes);
+app.use('/', requiresAuth(), routes);
 
 // Start server + connect to DB
 initDb((err) => {
