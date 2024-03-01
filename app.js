3d50d1109e9abb7ae167cd49c1891943cd6cb72c
kerokero014 <kevinjrm.kr@gmail.com>
2024-02-29 18:58:53 -0700

    Update authentication configuration and routes

--- app.js
+++ app.js
@@ -3,11 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const { initDb } = require('./db/connect');
 const routes = require('./routes');
-//const { auth, requiresAuth } = require('express-openid-connect');
-
-//const jwksRsa = require('jwks-rsa');
-//const jwt = require('jsonwebtoken');
-
+const { auth, requiresAuth } = require('express-openid-connect');
 require('dotenv').config();
 
 const app = express();
@@ -17,23 +13,23 @@ const port = process.env.PORT || 8080;
 app.use(cors());
 app.use(bodyParser.json());
 
-//// Auth0 Configuration
-//const config = {
-//  authRequired: false,
-//  auth0Logout: true,
-//  secret: process.env.SECRET,
-//  baseURL: process.env.BASE_URL,
-//  clientID: process.env.CLIENT_ID,
-//  issuerBaseURL: process.env.ISSUER_BASE
-//};
+// Auth0 Configuration
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE
+};
 
 // Routes
+app.use(auth(config));
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
 
-//app.use(auth(config));
-//app.get('/', (req, res) => {
-//  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-//});
-app.use('/' , routes);
+app.use('/', requiresAuth() ,routes);
 
 //requiresAuth()
 // Start server & connect to DB
