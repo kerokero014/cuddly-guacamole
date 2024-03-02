869928db264f7d237a92d78c8f6476b80e52c734
kerokero014 <kevinjrm.kr@gmail.com>
2024-03-02 12:50:49 -0700

    Fix missing newline at end of file in swagger.js

--- routes/swagger.js
+++ routes/swagger.js
@@ -3,6 +3,7 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger-output.json');
 
 router.use('/api-docs', swaggerUi.serve);
+
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
