b98277b722fa152660a6299c71a6a9316a1509ce
kerokero014 <kevinjrm.kr@gmail.com>
2024-03-02 21:01:50 -0700

    Update user's first and last name

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -2,15 +2,16 @@ const express = require('express');
 const router = express.Router();
 const appValidator = require('../validation/app-validation');
 const applicationController = require('../controller/applicationController');
+const SingleValid = require('../validation/idValidation');
 
 router.get('/', applicationController.getAllApplications);
 
-router.get('/:id', applicationController.getApplicationbyId);
+router.get('/:id', SingleValid.idValidation(),applicationController.getApplicationbyId);
 
 router.post('/:id', appValidator.appRules(), applicationController.createApplicationbyUserId);
 
 router.put('/:id', appValidator.appRules(), applicationController.updateApplicationbyId);
 
-router.delete('/:id', applicationController.deleteApplicationbyId);
+router.delete('/:id', SingleValid.idValidation(), applicationController.deleteApplicationbyId);
 
 module.exports = router;
