2c007419048355233c37b8227840ab319b8b6ad7
kerokero014 <kevinjrm.kr@gmail.com>
2024-03-02 21:18:34 -0700

    Refactor idValidation import in applicationRoutes.js

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -2,16 +2,16 @@ const express = require('express');
 const router = express.Router();
 const appValidator = require('../validation/app-validation');
 const applicationController = require('../controller/applicationController');
-const SingleValid = require('../validation/idValidation');
+const SingleV = require('../validation/idValidation');
 
 router.get('/', applicationController.getAllApplications);
 
-router.get('/:id', SingleValid.idValidation(),applicationController.getApplicationbyId);
+router.get('/:id', SingleV.idValidation(), applicationController.getApplicationbyId);
 
 router.post('/:id', appValidator.appRules(), applicationController.createApplicationbyUserId);
 
 router.put('/:id', appValidator.appRules(), applicationController.updateApplicationbyId);
 
-router.delete('/:id', SingleValid.idValidation(), applicationController.deleteApplicationbyId);
+router.delete('/:id', SingleV.idValidation(), applicationController.deleteApplicationbyId);
 
 module.exports = router;
